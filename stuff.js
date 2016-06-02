d0885f5b3a8eac6a07d87aab956fc107ff105b3e
Yazhini <yazhini@zeta.in>
2016-06-03 00:38:12 +0530

    fix IE bug

--- stuff.js
+++ stuff.js
@@ -101,3 +101,7 @@ Array.prototype.each_slice = function (size, callback){
 	}
 };
 
+String.prototype.endsWith = function(pattern) {
+  var d = this.length - pattern.length;
+  return d >= 0 && this.lastIndexOf(pattern) === d;
+};
